2c205c085970383157c03b543d3b3292ed8c34f6
小川貴大 <mistolteen0127@gmail.com>
2019-07-15 22:16:05 +0900

    タイトルとナビゲーションタブバー

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,27 @@
 import React from "react"
-import PropTypes from "prop-types"
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles"
-import AppBar from "@material-ui/core/AppBar"
-import Tabs from "@material-ui/core/Tabs"
-import Tab from "@material-ui/core/Tab"
-import Typography from "@material-ui/core/Typography"
+import { AppBar, Tabs, Tab, Toolbar, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
 
-interface TabContainerProps {
-  children?: React.ReactNode
-}
-
-function TabContainer(props: TabContainerProps) {
-  return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {props.children}
-    </Typography>
-  )
-}
-
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired
-}
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      backgroundColor: theme.palette.background.paper
-    }
-  })
-)
+const useStyles = makeStyles({
+  flex: {
+    flex: 1
+  }
+})
 
 export default function SimpleTabs() {
   const classes = useStyles()
-  const [value, setValue] = React.useState(0)
-
-  function handleChange(event: React.ChangeEvent<{}>, newValue: number) {
-    setValue(newValue)
-  }
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs value={value} onChange={handleChange}>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" className={`${classes.flex}`}>
+          My Portfolio!
+        </Typography>
+        <Tabs value={0}>
           <Tab label="Item One" />
           <Tab label="Item Two" />
           <Tab label="Item Three" />
         </Tabs>
-      </AppBar>
-      {value === 0 && <TabContainer>Item One</TabContainer>}
-      {value === 1 && <TabContainer>Item Two</TabContainer>}
-      {value === 2 && <TabContainer>Item Three</TabContainer>}
-    </div>
+      </Toolbar>
+    </AppBar>
   )
 }
