3891539f0b2d28d4a893d82066f1ef9bda951712
小川貴大 <mistolteen0127@gmail.com>
2019-07-16 00:05:03 +0900

    flexboxとアイコン表示

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,33 @@
 import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import { Box } from "@material-ui/core"
+import icon from "../assets/icon.jpg"
+
+const useStyles = makeStyles({
+  profile: {
+    flexGrow: 2
+  },
+  biography: {
+    flexGrow: 3
+  }
+})
 
 type HomeProps = {
   text?: string
 }
 
 export default function Home({ text = "Home" }: HomeProps) {
-  return <p>{text}</p>
+  const classes = useStyles()
+  return (
+    <>
+      <Box display="flex" padding={1} bgcolor="background.paper">
+        <Box padding={1} flexGrow={2} bgcolor="grey.300">
+          <img src={icon} />
+        </Box>
+        <Box padding={1} flexGrow={3} bgcolor="grey.300">
+          Item 2
+        </Box>
+      </Box>
+    </>
+  )
 }
