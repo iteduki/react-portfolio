90482ce3f3bdb2a2ac74d55584c1f8d42e4bca1c
小川貴大 <mistolteen0127@gmail.com>
2019-07-15 22:52:47 +0900

    クリックで選択できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,29 @@ import { makeStyles } from "@material-ui/core/styles"
 const useStyles = makeStyles({
   flex: {
     flex: 1
+  },
+  title: {
+    cursor: "pointer"
   }
 })
 
 export default function SimpleTabs() {
   const classes = useStyles()
+  const [value, setValue] = React.useState(0)
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue)
+  }
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" className={`${classes.flex}`}>
+        <Typography
+          variant="h6"
+          className={`${classes.flex} ${classes.title}`}
+          onClick={() => setValue(0)}
+        >
           My Portfolio!
         </Typography>
-        <Tabs value={0}>
+        <Tabs value={value} onChange={handleChange}>
           <Tab label="Item One" />
           <Tab label="Item Two" />
           <Tab label="Item Three" />
