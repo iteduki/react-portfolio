85959cb176e991fcbda4465f712492ebd91268f0
小川貴大 <mistolteen0127@gmail.com>
2019-07-20 20:30:48 +0900

    記事jsonを読み込んで表示する

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,5 +1,13 @@
 import React from "react"
 import { Box, Typography, makeStyles } from "@material-ui/core"
+import contents from "./WorksComponent/contents.json"
+
+type Contents = {
+  title: string
+  tools: string
+  description: string
+  href?: string
+}
 
 const useStyles = makeStyles({
   works: {
@@ -15,6 +23,11 @@ export default function Works() {
   return (
     <Box className={classes.works}>
       <Typography variant="h1">Works</Typography>
+      <Box style={{ display: "flex" }}>
+        {contents.map((item: Contents, key) => {
+          return <Typography>{item.title}</Typography>
+        })}
+      </Box>
     </Box>
   )
 }
