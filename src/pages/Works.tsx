0333962ad06a97aabd03d8bff9e5d310be312bd5
小川貴大 <mistolteen0127@gmail.com>
2019-07-20 20:20:51 +0900

    Worksページタイトル

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,9 +1,20 @@
 import React from "react"
+import { Box, Typography, makeStyles } from "@material-ui/core"
 
-type WorksProps = {
-  text?: string
-}
+const useStyles = makeStyles({
+  works: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    margin: 20
+  }
+})
 
-export default function Works({ text = "Works" }: WorksProps) {
-  return <p>{text}</p>
+export default function Works() {
+  const classes = useStyles()
+  return (
+    <Box className={classes.works}>
+      <Typography variant="h1">Works</Typography>
+    </Box>
+  )
 }
