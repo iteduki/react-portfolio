4a4353dc8d3719b390113ecb1b0c10a2d4fb708b
小川貴大 <mistolteen0127@gmail.com>
2019-07-22 13:21:07 +0900

    職歴ページタイトルの表示

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,9 +1,21 @@
 import React from "react"
+import { Box, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/styles"
 
-type ResumeProps = {
-  text?: string
-}
+const useStyles = makeStyles({
+  resume: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    margin: 20
+  }
+})
 
-export default function Resume({ text = "Resume" }: ResumeProps) {
-  return <p>{text}</p>
+export default function Resume() {
+  const classes = useStyles()
+  return (
+    <Box className={classes.resume}>
+      <Typography variant="h2">Resume</Typography>
+    </Box>
+  )
 }
