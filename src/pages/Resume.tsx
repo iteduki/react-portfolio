29a813c442d3d32112927b1bdab4e8b4a5f4bada
小川貴大 <mistolteen0127@gmail.com>
2019-07-23 08:38:03 +0900

    Resumeのレイアウトを決定

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, Typography } from "@material-ui/core"
+import { Box, Typography, Card, CardContent } from "@material-ui/core"
 import { makeStyles } from "@material-ui/styles"
 
 const useStyles = makeStyles({
@@ -8,7 +8,12 @@ const useStyles = makeStyles({
     flexDirection: "column",
     alignItems: "center",
     margin: 20
-  }
+  },
+  card: {
+    width: "100%",
+    maxWidth: 1200
+  },
+  skillCard: { margin: 8 }
 })
 
 export default function Resume() {
@@ -16,6 +21,36 @@ export default function Resume() {
   return (
     <Box className={classes.resume}>
       <Typography variant="h2">Resume</Typography>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="h4">都内 SIer</Typography>
+          <ul>
+            <li>
+              <Typography variant="body1">受託開発</Typography>
+            </li>
+            <li>
+              <Typography variant="body1">2018/7 ~</Typography>
+            </li>
+          </ul>
+          <Typography variant="h5">技術</Typography>
+          <Box style={{ display: "flex" }}>
+            <Card className={classes.skillCard}>
+              <CardContent>
+                <Typography variant="body1">Ruby on Rails</Typography>
+              </CardContent>
+            </Card>
+            <Card className={classes.skillCard}>
+              <CardContent>
+                <Typography variant="body1">React Native</Typography>
+              </CardContent>
+            </Card>
+          </Box>
+          <Typography variant="h5" gutterBottom>
+            コメント
+          </Typography>
+          <Typography variant="body1">毎日頑張りました</Typography>
+        </CardContent>
+      </Card>
     </Box>
   )
 }
